e0474d0fbb7a066e59590fab76c4e5661c5d5cb3
Степан Юшин <stepanyushin@gmail.com>
2023-10-03 21:13:37 +0700

    add config

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,36 @@
+import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {BuildOptions} from "./types/config";
+
+export function buildLoaders (options: BuildOptions): webpack.RuleSetRule[] {
+
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            options.isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+                loader: "css-loader",
+                options: {
+                    modules: {
+                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+                        localIdentName: options.isDev
+                            ? '[path][name]__[local]--[hash:base64:5]'
+                            : '[hash:base64:8]'
+                    }
+                }
+            },
+            "sass-loader",
+        ],
+    }
+
+    const typeSkriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    }
+
+    return [
+        typeSkriptLoader,
+        cssLoader,
+    ]
+}
