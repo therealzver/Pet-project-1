4773439c4dfac4afdf9c52ec6758ee6acb3fd8fe
Степан Юшин <stepanyushin@gmail.com>
2023-11-03 18:19:42 +0700

    add loading and NoFoundPage

---
+++ src/shared/ui/Loader/Loader.tsx
@@ -0,0 +1,13 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import './Loader.scss';
+
+interface LoaderProps {
+    className?: string,
+}
+export const Loader = ({ className }: LoaderProps) => (
+    <div className={classNames('lds-facebook', {}, [className])}>
+        <div />
+        <div />
+        <div />
+    </div>
+);
